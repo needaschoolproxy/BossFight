925518e3cc20439234003788039567cdfabfc1f2
SomeIdito <luca.g.burns@icloud.com>
2025-08-28 20:24:01 -0400

    oh the misery
    i added some attacks and an enemy

---
+++ scenes/enemy.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var health := 300
+var knockback_velocity := Vector2.ZERO
+const GRAVITY := 2000.0
+const HURT_DURATION := 0.25
+var is_hurt := false
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.y = 0.0
+
+	if knockback_velocity.length() > 10.0:
+		velocity = knockback_velocity
+		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, 1000.0 * delta)
+	else:
+		knockback_velocity = Vector2.ZERO
+
+	move_and_slide()
+
+	if health <= 0:
+		queue_free()
+		return
+
+	if not is_hurt and sprite.animation != "Idle":
+		sprite.play("Idle")
+
+func take_damage(damage: int, knockback: Vector2) -> void:
+	health -= damage
+	knockback_velocity = knockback
+
+	if health <= 0:
+		queue_free()
+		return
+
+	var frames := sprite.sprite_frames
+	if frames and frames.has_animation("Hurt"):
+		is_hurt = true
+		sprite.play("Hurt")
+		await get_tree().create_timer(HURT_DURATION).timeout
+		is_hurt = false
+	else:
+		is_hurt = true
+		var original_modulate := sprite.modulate
+		sprite.modulate = Color(1, 0.5, 0.5)
+		await get_tree().create_timer(HURT_DURATION).timeout
+		sprite.modulate = original_modulate
+		is_hurt = false
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,42 +1,124 @@
 extends CharacterBody2D
+
 @onready var dash_time: Timer = $DashTime
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
-var SPEED = 350.0
-const JUMP_VELOCITY = -500.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_area: Area2D = $AttackArea
+
+var SPEED = 450.0
+const JUMP_VELOCITY = -900.0
+const GRAVITY = 2000.0
+const FAST_FALL_MULTIPLIER = 2.0
+const JUMP_CUT_MULTIPLIER = 0.5
 var doublejumped = false
+
 const DASH_SPEED = 1200
 var dashing = false
 var can_dash = true
 
-func _physics_process(delta: float) -> void: 
+var attacking = false
+var ATTACK_DAMAGE = 20
+var ATTACK_KNOCKBACK = 400.0
+var can_attack = true
+const ATTACK_COOLDOWN = 0.5
+
+var jumping = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack") and not attacking and can_attack:
+		start_attack()
+
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	else: if Input.is_action_just_pressed("jump") and doublejumped == false:
-		velocity.y = JUMP_VELOCITY
-		doublejumped = true
+		if velocity.y > 0:
+			velocity.y += GRAVITY * FAST_FALL_MULTIPLIER * delta
+		else:
+			velocity.y += GRAVITY * delta
+	else:
+		velocity.y = 0
+		if jumping:
+			jumping = false
+
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			trigger_jump_anim()
+		elif not doublejumped:
+			velocity.y = JUMP_VELOCITY
+			doublejumped = true
+			trigger_jump_anim()
+
+	if not Input.is_action_pressed("jump") and velocity.y < 0:
+		velocity.y *= JUMP_CUT_MULTIPLIER
+
 	if is_on_floor():
 		doublejumped = false
-		
+
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		if dashing == true:
+	if direction != 0:
+		if dashing:
 			velocity.x = direction * DASH_SPEED
 		else:
 			velocity.x = direction * SPEED
+		sprite.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_just_pressed("dash") and can_dash == true:
+		velocity.x = move_toward(velocity.x, 0, SPEED * 2 * delta)
+
+	if attacking:
+		if sprite.animation != "Attack":
+			sprite.play("Attack")
+	elif dashing:
+		if sprite.animation != "Dash":
+			sprite.play("Dash")
+	elif jumping:
+		if sprite.animation != "Jump":
+			sprite.play("Jump")
+	elif direction != 0:
+		if sprite.animation != "MoveLeftRight":
+			sprite.play("MoveLeftRight")
+	else:
+		if sprite.animation != "Idle":
+			sprite.play("Idle")
+
+	if Input.is_action_just_pressed("dash") and can_dash and not attacking:
 		dashing = true
 		can_dash = false
-		$DashTime.start()
-		$DashCooldownTimer.start()
+		dash_time.start()
+		dash_cooldown_timer.start()
+
 	move_and_slide()
-	
+
+func start_attack():
+	attacking = true
+	can_attack = false
+	attack_area.monitoring = true
+	$AttackArea/CollisionShape2D.disabled = false
+
+	if not attack_area.is_connected("body_entered", Callable(self, "_on_attack_area_body_entered")):
+		attack_area.connect("body_entered", Callable(self, "_on_attack_area_body_entered"))
+
+	await get_tree().create_timer(0.1).timeout
+	attack_area.monitoring = false
+	$AttackArea/CollisionShape2D.disabled = true
+
+	attacking = false
+
+	await get_tree().create_timer(ATTACK_COOLDOWN).timeout
+	can_attack = true
+
+func _on_attack_area_body_entered(body):
+	if body.has_method("take_damage"):
+		var knock_dir = -1 if sprite.flip_h else 1
+		body.take_damage(ATTACK_DAMAGE, Vector2(knock_dir * ATTACK_KNOCKBACK, -200))
+
+		velocity.x = -knock_dir * 100
+
+func trigger_jump_anim():
+	jumping = true
+	if sprite.animation != "Jump":
+		sprite.play("Jump")
+
 func _on_dash_time_timeout() -> void:
 	dashing = false
-	
+
 func _on_dash_cooldown_timer_timeout() -> void:
 	can_dash = true
