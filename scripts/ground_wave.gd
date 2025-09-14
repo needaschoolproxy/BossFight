1d94249e4ca9dbcdcd86be58d407187e8a6fe9ac
GlaggleGuy <mylesanathan@gmail.com>
2025-09-14 11:58:54 -0400

    lightning sprite and flipping

--- scripts/ground_wave.gd
+++ scripts/ground_wave.gd
@@ -2,20 +2,23 @@ extends RigidBody2D
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var collision_polygon_2d: CollisionPolygon2D = $Area2D/CollisionPolygon2D
-@onready var groundwave: Sprite2D = $Groundwave
+@onready var groundwavesprite: Sprite2D = $Groundwavesprite
+
 
 const SPEED: int = 500
-var flip: bool = false
-const DAMAGE: int = 15
+var flip = false
+const DAMAGE: int = 9
 const KNOCKBACK: float = 600.0
 
-func _ready() -> void:
-	if flip:
-		flipped()
 
 func _process(delta: float) -> void:
 	var dir := -1 if flip else 1
 	position += transform.x * SPEED * dir * delta
+	if flip:
+		$Groundwavesprite.flip_h = true
+		collision_shape_2d.scale.x = -1
+		collision_polygon_2d.scale.x = -1
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
@@ -23,7 +26,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		body.take_damage(DAMAGE, Vector2(knock_dir * KNOCKBACK, -200))
 	queue_free()
 
-func flipped() -> void:
-	groundwave.scale.x = -groundwave.scale.x
-	collision_shape_2d.position.x *= -1
-	collision_polygon_2d.position.x *= -1
+
+	
--- scripts/lightning.gd
+++ scripts/lightning.gd
@@ -2,12 +2,13 @@ extends Area2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if not $RayCast2D.is_colliding():
 		position.y += 1
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
