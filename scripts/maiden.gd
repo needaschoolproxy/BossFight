82ac43e94fb848dcb4df8cdd80ea46d49f02becf
GlaggleGuy <mylesanathan@gmail.com>
2025-09-21 21:57:30 -0400

    ojuioph

--- scripts/maiden.gd
+++ scripts/maiden.gd
@@ -25,15 +25,17 @@ func _process(delta: float) -> void:
 		state.stomp:
 			position.y = lerp(position.y,character_body_2d.position.y,STOMP_SPEED * delta)
 			
+			
 	if health <= 0: return queue_free()
 	move_and_slide()
 	
 
-
 func _on_timer_timeout() -> void:
-	current_state = state.stomp
-	await get_tree().create_timer(1).timeout
-	current_state = state.idle
+	for i in 3:
+		current_state = state.stomp
+		await get_tree().create_timer(0.85).timeout
+		current_state = state.idle
+		await get_tree().create_timer(0.75).timeout
 
 
 func take_damage(dmg: int, kb: Vector2) -> void:
@@ -44,3 +46,7 @@ func take_damage(dmg: int, kb: Vector2) -> void:
 	await get_tree().create_timer(HURT_DURATION).timeout
 	$Icon.modulate = Color(1,1,1)
 	is_hurt = false
+
+
+func _on_area_2d_body_entered() -> void:
+	$"../CharacterBody2D".health -= 6
